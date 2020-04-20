b71055cca379a9bbde628971e763b512cbd33891
Ederson Schaukoski <edersonschaukoski@gmail.com>
2020-04-20 12:28:51 -0300

    Added server.js

---
+++ build/precache-manifest.c4b6e1f4835d24f486702b1b96ba032b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83d25853e250333c117cd227b190a72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d55d61f6d243a0db43de",
+    "url": "/static/js/2.f3b37a1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f3b37a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b25bf49078ff3423b7f",
+    "url": "/static/js/main.0f9e3eb6.chunk.js"
+  },
+  {
+    "revision": "68823497c106830c3575",
+    "url": "/static/js/runtime-main.7a808348.js"
+  }
+]);
